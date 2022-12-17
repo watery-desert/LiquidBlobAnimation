c7848e1c4869ef624e21a2df91123dcfd0466fdb
Ahmed <waterydesert.developer@gmail.com>
2022-12-17 17:38:02 +0530

    Initial Commit

---
+++ LiquidBlobAnimation/LiquidBlobAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidBlobAnimationApp.swift
+//  LiquidBlobAnimation
+//
+//  Created by Ahmed on 17/12/22.
+//
+
+import SwiftUI
+
+@main
+struct LiquidBlobAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
